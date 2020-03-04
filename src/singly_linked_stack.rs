b28f800bc817d19d32c9f2b9aab9907550501fcd
Jihoon Kim <kjh618@kaist.ac.kr>
2020-03-04 16:59:07 +0900

    Implement basic functions

--- src/singly_linked_stack.rs
+++ src/singly_linked_stack.rs
@@ -9,15 +9,20 @@ pub struct Node<T> {
 
 impl<T> List<T> {
     pub fn new() -> Self {
-        unimplemented!();
+        List { head: None }
     }
 
     pub fn push(&mut self, elem: T) {
-        unimplemented!();
+        let new_node = Node { elem: elem, next: self.head.take() };
+        self.head = Some(Box::new(new_node));
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        unimplemented!();
+        let old_head_opt = self.head.take();
+        old_head_opt.map(|old_head| {
+            self.head = old_head.next;
+            old_head.elem
+        })
     }
 }
 
